914907fb23ca95b3197dc920e0f52cd378c6e9ce
eloy411 <91909651+eloy411@users.noreply.github.com>
2022-11-14 16:05:51 +0100

    insert room
    insert room with the ligths and camera scene properties

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 
 import * as THREE from 'three';
+import Room from './room.js';
 import { OrbitControls } from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/loaders/GLTFLoader.js';
 
@@ -8,7 +9,14 @@ const scene = new THREE.Scene();
 // PerspectiveCamera is te most used one (There is a lot of cameras). Because represent human eye
 // params = (Field of view, aspect ratio (user window), view frustum)
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
+
+
+const helper = new THREE.CameraHelper( camera );
+
+
+scene.add( helper );
+
+
 
 // Render the scece in the element that you want. In our case is a canvas
 const renderer =  new THREE.WebGLRenderer({
@@ -19,10 +27,10 @@ renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 //position the camera in the scene
-camera.position.setZ(30);
+// camera.position.setZ(30);
 
 // Controls to move in the scene
-const controls = new OrbitControls( camera, renderer.domElement );
+// const controls = new OrbitControls( camera, renderer.domElement );
 
 
 // pointLightHelper to know where is the light source
@@ -39,12 +47,44 @@ window.addEventListener('resize', function() {
     renderer.setSize(window.innerWidth, window.innerHeight);
 })
 
+// camera.position.setX = 10
+camera.position.z = 9
+camera.position.y = 11
+camera.position.x = 25
+camera.rotation.x = -0.15
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
 
+// const room = new Room(scene);
+// room.loadRoom()
+
+const loader = new GLTFLoader();
+loader.load(
+    // resource URL
+    'design/room.glb',
+    // called when the resource is loaded
+    function ( gltf ) {
+
+        scene.add( gltf.scene );
+
+        gltf.animations; // Array<THREE.AnimationClip>
+        gltf.scene; // THREE.Group
+        gltf.scenes; // Array<THREE.Group>
+        gltf.cameras; // Array<THREE.Camera>
+        gltf.asset; // Object
+        
+
+    })
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+const light = new THREE.AmbientLight( 0xffffff ); // soft white light
+scene.add( light );
+
+//////////////////////////////////////////////////////////////////
+
 function animate() {
 
-    controls.update();
+    // controls.update();
     renderer.render(scene, camera)
     requestAnimationFrame(animate)
 }
